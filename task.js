aaa218a8b0288ac30e950f279d89a271366b1615
SelmaMammedli <selma.mammedli@gmail.com>
2023-03-11 23:47:30 +0400

    GIT task

---
+++ task.js
@@ -0,0 +1,10 @@
+var num=[2,4,7,8,18,12];
+let sum=0;
+let count=0;
+for (let index = 0; index < num.length; index++) {
+    if ( num%3==0) ;
+    sum+=num;
+    
+    
+}
+console.log(sum/num.length)
